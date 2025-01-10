aa8ca8308ed161d39e44997c128f787e5e0634e2
Ali-Akber Raza <aaoblivion@gmail.com>
2025-01-10 10:25:52 -0500

    added stage switchibng

--- src/routes/regression/+page.svelte
+++ src/routes/regression/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 import { onMount } from 'svelte';
-import { goto } from '$app/navigation';
 let innerWidth = $state(0);
 let onPC = $derived(innerWidth > 768);
 let titleVisible = $state(false);
 let subtitleVisible = $state(false);
 let cards: boolean [] = $state([]);
-cards.push(false);
-cards.push(false);
-cards.push(false);
-cards.push(false);
-cards.push(false);
-cards.push(false);
+let stages: boolean [] = $state([true]);
+let firstDiv = $state(true);
+let stagesPtr = 0;
+let chosenFunction: string;
+for (let index = 0; index < 6; index++) {
+  cards.push(false);
+}
+for (let index = 0; index < 2; index++) {
+  stages.push(false);
+}
 onMount(() => {
   setTimeout(() => {
     titleVisible = true;
@@ -25,15 +28,29 @@ onMount(() => {
     }, 1500 + (i + 1) * 500);
   }
 });
+function nextStage() {
+  stages[stagesPtr] = false;
+  stagesPtr++;
+  stages[stagesPtr] = true;
+}
+function chooseFunction(functionType: string) {
+  chosenFunction = functionType;
+  nextStage();
+  setTimeout(() => {
+    firstDiv = false;
+  }, 1000);
+}
 </script>
 <svelte:window bind:innerWidth />
+{#if firstDiv}
+<div id="firstStage" class:fade-in={stages[0]} class:hidden={!stages[0]}>
 <div class="goodDiv">
   <p id="pageTitle" class:fade-in={titleVisible} class:hidden={!titleVisible}>ModelViz</p>
-  <p id="pageSubtitle" class:fade-in={subtitleVisible} class:hidden={!subtitleVisible}>Choose a problem to visualize</p>
+  <p id="pageSubtitle" class:fade-in={subtitleVisible} class:hidden={!subtitleVisible}>Choose a function to model</p>
 </div>
 <div class="allCardContainer">
   <div class="card-container">
-    <div class="card" class:fade-in={cards[0]} class:hidden={!cards[0]}>
+    <div class="card" class:fade-in={cards[0]} class:hidden={!cards[0]} onclick={() => chooseFunction("sine")}>
       <svg viewBox="0 0 200 250" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
         <g stroke="lightgrey" stroke-width="0.5">
           <!-- Vertical gridlines -->
@@ -75,11 +92,11 @@ onMount(() => {
         <path d="M0,125 Q25,50 50,10 T100,125 T150,240 T190,125" 
           stroke="#eb4034" fill="none" stroke-width="2" />
       </svg>
-      <p class="card-text">Sine Function</p>
+      <p class="card-text">Sine</p>
     </div>
   </div>
   <div class="card-container">
-    <div class="card" class:fade-in={cards[1]} class:hidden={!cards[1]}> 
+    <div class="card" class:fade-in={cards[1]} class:hidden={!cards[1]} onclick={() => chooseFunction("parabola")}> 
       <svg viewBox="0 0 200 250" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
         <g stroke="lightgrey" stroke-width="0.5">
           <!-- Vertical gridlines -->
@@ -129,7 +146,7 @@ onMount(() => {
   </div>
 
   <div class="card-container">
-    <div class="card" class:fade-in={cards[2]} class:hidden={!cards[2]}>
+    <div class="card" class:fade-in={cards[2]} class:hidden={!cards[2]} onclick={() => chooseFunction("exponential")}>
       <svg viewBox="0 0 200 250" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
         <g stroke="lightgrey" stroke-width="0.5">
           <!-- Vertical gridlines -->
@@ -169,114 +186,12 @@ onMount(() => {
 <path d="M200 0 Q 150 90, 110 140 Q 70 190, 0 250"
           stroke="#eb4034" stroke-width="2" fill="none" />
       </svg>
-      <p class="card-text">Exponential Function</p>
-    </div>
-  </div>
-  <div class="card-container">
-    <div class="card" class:fade-in={cards[3]} class:hidden={!cards[3]}> 
-      <svg viewBox="0 0 200 250" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
-        <g stroke="lightgrey" stroke-width="0.5">
-          <!-- Vertical gridlines -->
-          <line x1="25" y1="0" x2="25" y2="250" />
-          <line x1="50" y1="0" x2="50" y2="250" />
-          <line x1="75" y1="0" x2="75" y2="250" />
-          <line x1="100" y1="0" x2="100" y2="250" />
-          <line x1="125" y1="0" x2="125" y2="250" />
-          <line x1="150" y1="0" x2="150" y2="250" />
-          <line x1="175" y1="0" x2="175" y2="250" />
-          <line x1="200" y1="0" x2="200" y2="250" />
-          <line x1="225" y1="0" x2="225" y2="250" />
-
-          <!-- Horizontal gridlines -->
-          <line x1="0" y1="1" x2="250" y2="1" />
-          <line x1="0" y1="25" x2="250" y2="25" />
-          <line x1="0" y1="50" x2="250" y2="50" />
-          <line x1="0" y1="75" x2="250" y2="75" />
-          <line x1="0" y1="100" x2="250" y2="100" />
-          <line x1="0" y1="125" x2="250" y2="125" />
-          <line x1="0" y1="150" x2="250" y2="150" />
-          <line x1="0" y1="175" x2="250" y2="175" />
-          <line x1="0" y1="200" x2="250" y2="200" />
-          <line x1="0" y1="225" x2="250" y2="225" />
-          <line x1="0" y1="250" x2="250" y2="250" />
-        </g>
-        <!-- Axes -->
-        <line x1="0" y1="250" x2="250" y2="250" stroke="white" stroke-width="2" /> <!-- X-axis -->
-        <line x1="0" y1="0" x2="0" y2="250" stroke="white" stroke-width="2" /> <!-- Y-axis -->
-
-        <!-- Blue Points -->
-        <circle cx="10" cy="30" r="4" fill="#3477eb" />
-        <circle cx="30" cy="50" r="4" fill="#3477eb" />
-        <circle cx="50" cy="20" r="4" fill="#3477eb" />
-        <circle cx="70" cy="40" r="4" fill="#3477eb" />
-        <circle cx="90" cy="60" r="4" fill="#3477eb" />
-        <circle cx="110" cy="30" r="4" fill="#3477eb" />
-        <circle cx="130" cy="50" r="4" fill="#3477eb" />
-        <circle cx="150" cy="20" r="4" fill="#3477eb" />
-
-
-        <circle cx="50" cy="140" r="4" fill="#eb4034" />
-        <circle cx="70" cy="120" r="4" fill="#eb4034" />
-        <circle cx="90" cy="160" r="4" fill="#eb4034" />
-        <circle cx="110" cy="130" r="4" fill="#eb4034" />
-        <circle cx="130" cy="170" r="4" fill="#eb4034" />
-      </svg>
-      <p class="card-text">3</p>
-    </div>
-  </div>
-  <div class="card-container">
-    <div class="card" class:fade-in={cards[4]} class:hidden={!cards[4]}> 
-      <svg viewBox="0 0 200 250" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
-        <g stroke="lightgrey" stroke-width="0.5">
-          <!-- Vertical gridlines -->
-          <line x1="25" y1="0" x2="25" y2="250" />
-          <line x1="50" y1="0" x2="50" y2="250" />
-          <line x1="75" y1="0" x2="75" y2="250" />
-          <line x1="100" y1="0" x2="100" y2="250" />
-          <line x1="125" y1="0" x2="125" y2="250" />
-          <line x1="150" y1="0" x2="150" y2="250" />
-          <line x1="175" y1="0" x2="175" y2="250" />
-          <line x1="200" y1="0" x2="200" y2="250" />
-          <line x1="225" y1="0" x2="225" y2="250" />
-
-          <!-- Horizontal gridlines -->
-          <line x1="0" y1="1" x2="250" y2="1" />
-          <line x1="0" y1="25" x2="250" y2="25" />
-          <line x1="0" y1="50" x2="250" y2="50" />
-          <line x1="0" y1="75" x2="250" y2="75" />
-          <line x1="0" y1="100" x2="250" y2="100" />
-          <line x1="0" y1="125" x2="250" y2="125" />
-          <line x1="0" y1="150" x2="250" y2="150" />
-          <line x1="0" y1="175" x2="250" y2="175" />
-          <line x1="0" y1="200" x2="250" y2="200" />
-          <line x1="0" y1="225" x2="250" y2="225" />
-          <line x1="0" y1="250" x2="250" y2="250" />
-        </g>
-        <!-- Axes -->
-        <line x1="0" y1="250" x2="250" y2="250" stroke="white" stroke-width="2" /> <!-- X-axis -->
-        <line x1="0" y1="0" x2="0" y2="250" stroke="white" stroke-width="2" /> <!-- Y-axis -->
-
-        <!-- Blue Points -->
-        <circle cx="10" cy="30" r="4" fill="#3477eb" />
-        <circle cx="30" cy="50" r="4" fill="#3477eb" />
-        <circle cx="50" cy="20" r="4" fill="#3477eb" />
-        <circle cx="70" cy="40" r="4" fill="#3477eb" />
-        <circle cx="90" cy="60" r="4" fill="#3477eb" />
-        <circle cx="110" cy="30" r="4" fill="#3477eb" />
-        <circle cx="130" cy="50" r="4" fill="#3477eb" />
-        <circle cx="150" cy="20" r="4" fill="#3477eb" />
-
-
-        <circle cx="50" cy="140" r="4" fill="#eb4034" />
-        <circle cx="70" cy="120" r="4" fill="#eb4034" />
-        <circle cx="90" cy="160" r="4" fill="#eb4034" />
-        <circle cx="110" cy="130" r="4" fill="#eb4034" />
-        <circle cx="130" cy="170" r="4" fill="#eb4034" />
-      </svg>
-      <p class="card-text">4</p>
+      <p class="card-text">Exponential</p>
     </div>
   </div>
 </div>
+</div>
+{/if}
 
 <style>
 @import "$lib/styles/main.css";
