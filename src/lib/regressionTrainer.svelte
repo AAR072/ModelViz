c1a9754b39ef97daffeeca3c26e8ee1e6ff0281c
Ali-Akber Raza <aaoblivion@gmail.com>
2025-01-10 22:27:13 -0500

    styling for model builder

--- src/lib/regressionTrainer.svelte
+++ src/lib/regressionTrainer.svelte
@@ -3,4 +3,11 @@ let { functionType, model } = $props();
 console.log(model)
 
 </script>
-<h1>{functionType}</h1>
+<div class="goodDiv">
+<h1>Training a {functionType} predictor</h1>
+</div>
+<style>
+@import '$lib/styles/main.css';
+
+
+</style>
